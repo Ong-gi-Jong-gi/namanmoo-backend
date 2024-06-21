8b023f7c048939925a2b2ed67163680c331956a2
unknown <kmj54321@cau.ac.kr>
2024-06-21 20:34:34 +0900

    first commit

---
+++ src/main/java/ongjong/namanmoo/NamanmooApplication.java
@@ -0,0 +1,13 @@
+package ongjong.namanmoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamanmooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamanmooApplication.class, args);
+	}
+
+}
