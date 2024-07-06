701ab21462ae70344a9dbc0255b59280851cc15d
minjoon-98 <4kmj54321@gmail.com>
2024-07-07 01:20:03 +0900

    setting : github action

---
+++ src/main/java/ongjong/namanmoo/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package ongjong.namanmoo.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class DatabaseConfig {
+
+    @Value("${SPRING_DATASOURCE_URL}")
+    private String url;
+
+    @Value("${SPRING_DATASOURCE_USERNAME}")
+    private String username;
+
+    @Value("${SPRING_DATASOURCE_PASSWORD}")
+    private String password;
+
+    @Value("${SECURITY_JWT_TOKEN_SECRET_KEY}")
+    private String secretKey;
+}
---
+++ src/main/java/ongjong/namanmoo/config/PropertyConfig.java
@@ -0,0 +1,13 @@
+package ongjong.namanmoo.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:env.properties")
+public class PropertyConfig {
+}
