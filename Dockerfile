95471ef6b99cbb3198ef439561394159ca78d35d
dongyeoppp <ds06041@gmail.com>
2024-07-24 03:17:07 +0900

    chore: fix firebase setting

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ ARG JAR_FILE=build/libs/*.jar
 
 # jar File Copy
 COPY ${JAR_FILE} mooluck-spring.jar
-# Firebase config file copy
-COPY src/main/resources/mooluck-fcm-firebase-adminsdk-cjwp5-cd30150b1a.json /app/resources/mooluck-fcm-firebase-adminsdk-cjwp5-cd30150b1a.json
+
 
 
 # 필요한 패키지 설치
@@ -16,7 +15,10 @@ RUN apt-get update && apt-get install -y ffmpeg
 ENV TZ=Asia/Seoul
 ENV FFMPEG_PATH=/usr/bin/ffmpeg
 ENV FFPROBE_PATH=/usr/bin/ffprobe
-#ENV FIREBASE_CONFIG_PATH=/app/resources/mooluck-fcm-firebase-adminsdk-cjwp5-cd30150b1a.json
+
+# Firebase config file copy
+COPY src/main/resources/mooluck-fcm-firebase-adminsdk-cjwp5-cd30150b1a.json /app/resources/mooluck-fcm-firebase-adminsdk-cjwp5-cd30150b1a.json
+
 
 # 기본 프로파일 실행 명령어
 ENTRYPOINT ["java", "-jar", "mooluck-spring.jar", "-Dspring.profiles.active=docker", "-Duser.timezone=Asia/Seoul"]
--- src/main/java/ongjong/namanmoo/config/FirebaseConfig.java
+++ src/main/java/ongjong/namanmoo/config/FirebaseConfig.java
@@ -1,38 +1,38 @@
-//package ongjong.namanmoo.config;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//
-//@Configuration
-//public class FirebaseConfig {
-//
-//    @Value("${firebase.config.path}")
-//    private String firebaseConfigPath;
-//
-//    @Bean
-//    public FirebaseApp initializeFirebase() throws IOException {
-//        if (FirebaseApp.getApps().isEmpty()) {
-//            InputStream serviceAccount;
-//            try {
-//                serviceAccount = new FileInputStream(firebaseConfigPath);
-//            } catch (Exception e) {
-//                throw new IOException("Could not open Firebase config file: " + firebaseConfigPath);
-//            }
-//
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .build();
-//
-//            return FirebaseApp.initializeApp(options);
-//        }
-//        return FirebaseApp.getInstance();
-//    }
-//}
+package ongjong.namanmoo.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+
+    @Value("${firebase.key-path}")
+    private Resource firebaseConfigPath;
+
+    @Bean
+    public FirebaseApp initializeFirebase() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(firebaseConfigPath.getInputStream()))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                log.info("complete:{}",firebaseConfigPath);
+                return FirebaseApp.initializeApp(options);
+            } else {
+                return FirebaseApp.getInstance();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize Firebase", e);
+        }
+    }
+}
