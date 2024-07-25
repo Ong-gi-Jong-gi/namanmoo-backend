e861f32a54034602fdefaa742bda871f09117d80
minjoon-98 <4kmj54321@gmail.com>
2024-07-26 01:03:26 +0900

    chore: to measure Response Time Synchronous

--- src/main/java/ongjong/namanmoo/controller/ChallengeController.java
+++ src/main/java/ongjong/namanmoo/controller/ChallengeController.java
@@ -330,8 +330,8 @@ public class ChallengeController {
     // 화상 통화 챌린지 결과 저장
     @PostMapping("/face")
     public ApiResponse<Map<String, String>> saveFaceTimeAnswer(
-        @RequestParam("challengeId") Long challengeId,
-        @RequestPart("answer") MultipartFile answerFile) throws Exception {
+            @RequestParam("challengeId") Long challengeId,
+            @RequestPart("answer") MultipartFile answerFile) throws Exception {
 
         ApiResponse<Challenge> challengeResponse = validateChallenge(challengeId, ChallengeType.FACETIME);
         if (!challengeResponse.getStatus().equals("200")) {
@@ -363,9 +363,21 @@ public class ChallengeController {
             // Answer 업데이트
             answerService.modifyAnswer(challengeId, uploadedUrl);
 
+    //            // TODO: 원래 하던 코드 (4번째 cut 사진이 병합이 안되는 현상 발생)
+    //            // 그룹별 4개의 이미지가 모였는지 확인 및 병합
+    //            sharedFileService.checkAndMergeImages(challenge.getChallengeNum(), lucky);
+
             // TODO: 방법 1: 이미지 업로드와 병합 분리 (4번째 cut 1장 만들어짐) -> 채택!
             // 이미지 업로드가 완료된 후에 병합을 시도합니다.
             sharedFileService.mergeImagesIfNeeded(challenge.getChallengeNum(), lucky);
+
+    //            // TODO: 방법 2: 병합을 서버 측에서 스케줄링 (첫번째 cut만 생성됨)
+    //            // 이미지 업로드가 완료된 후에 병합을 예약합니다.
+    //            sharedFileService.scheduleMergeImages(challenge.getChallengeNum(), lucky);
+
+    //            // TODO: 비동기 시도...
+    //            // 비디오 업로드 후 이미지 병합 작업 스케줄링
+    //            sharedFileService.scheduleMergeImages(challenge.getChallengeNum(), lucky);
 
             return new ApiResponse<>("200", "Video uploaded successfully", Map.of("url", uploadedUrl));
         } else {
